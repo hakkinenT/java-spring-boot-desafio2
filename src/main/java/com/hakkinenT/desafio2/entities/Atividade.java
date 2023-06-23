e64ff75e1f4d13dc1089c9dee553758b4a5779dc
Tawanna Hakkinen <thol_16@hotmail.com>
2023-06-23 13:20:28 -0300

    Entidade Participante, relacionamento muitos-para-muitos com Atividade

--- src/main/java/com/hakkinenT/desafio2/entities/Atividade.java
+++ src/main/java/com/hakkinenT/desafio2/entities/Atividade.java
@@ -2,9 +2,7 @@ package com.hakkinenT.desafio2.entities;
 
 import jakarta.persistence.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_atividade")
@@ -22,6 +20,12 @@ public class Atividade {
 
     @OneToMany(mappedBy = "atividade")
     private List<Bloco> blocos = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_atividade_participante",
+            joinColumns = @JoinColumn(name = "atividade_id"),
+            inverseJoinColumns = @JoinColumn(name = "participante_id"))
+    private Set<Participante> participantes = new HashSet<>();
 
     public Atividade() {
     }
@@ -76,6 +80,10 @@ public class Atividade {
 
     public List<Bloco> getBlocos() {
         return blocos;
+    }
+
+    public Set<Participante> getParticipantes() {
+        return participantes;
     }
 
     @Override
